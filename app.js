7e5db4f9a00764aa8b77ac883718b93c0b794b00
Javalind21 <javaughnlindsay21@gmail.com>
2023-07-23 19:23:47 -0400

    Project 4 Pokedex complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const pokeBank = require("./pokeBank");
 const pokeList = require("./views/pokeList");
 const pokeDetails = require("./views/pokeDetails");
+const Pokemon = require("./Pokemon");
 
 const app = express();
 
@@ -14,9 +15,43 @@ app.get("/", (req, res) => {
   res.send(pokeList(pokemon));
 });
 
-app.get("/pokemon/:id", (req, res) => {
-  const pokemon = pokeBank.find(req.params.id);
-  res.send(pokeDetails(pokemon));
+app.get("/pokemon", async (req, res) => {
+  const pokemon = await Pokemon.findAll();
+  res.json(pokemon);
+});
+
+app.post("/pokemon", async (req, res) => {
+  const newPokemon = await Pokemon.create(req.body);
+  res.json(newPokemon);
+});
+
+app.get("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+
+app.put("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.update(req.body);
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+
+app.delete("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.destroy();
+    res.status(204).send();
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
 });
 
 const PORT = 1337;
